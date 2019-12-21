c0fa36982c91263b769e3911fa01478dae3979e7
Romanchuk Serhii <romanchuk.sss22121999@gmail.com>
2019-12-21 11:43:30 +0200

    static resource and dinamizatsiya spisku studentiv, shabloni django

--- students/views.py
+++ students/views.py
@@ -4,11 +4,31 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
-#Views for students
+#Views for students 
 
 def students_list(request):
-    return render(request, 'students/students_list.html',{})
-    
+	students = (
+		{'id': 1,
+		'first_name': u'Сергій',
+		'last_name': u'Романчук',
+		'ticket': 3,
+		'image': 'img/me.jpg'},
+		{'id': 2,
+		 'first_name': u'Максим',
+		 'last_name': u'Романчук',
+		 'ticket': 6,
+		 'image': 'img/max.jpg',
+		},
+		{'id': 3,
+		 'image': 'img/podoba3.jpg',
+		 'first_name': u'Віталій',
+		 'last_name': u'Подоба',
+		 'ticket': 9
+		},	
+	)
+	
+	return render (request,'students/students_list.html', {'students': students})
+		
 def students_add(request):
     return HttpResponse('<h1> Students Add Form </h1>')
     
@@ -17,6 +37,8 @@ def students_edit(request, sid):
     
 def students_delete(request, sid):
     return HttpResponse('<h1> Delete Student %s </h1>' % sid)
+    
+
 
 #Views for groups    
 def groups_list(request):
@@ -30,6 +52,7 @@ def groups_edit(request, gid):
     
 def groups_delete(request, gid):
     return HttpResponse('<h1> Delete Group %s </h1>'% gid)
+    
 
 #HttpResponse('<h1> Romanchuk Serhii! </h1>')
 
