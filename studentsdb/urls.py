73ee77fcfede1a53adb5ca11d62d6d3f61fb3a79
Romanchuk Serhii <romanchuk.sss22121999@gmail.com>
2020-02-29 17:02:16 +0200

    journal for one student

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     
 
     #Journal urls
-    url(r'^journal/$', JournalView.as_view(), name='journal'),
+    url(r'^journal/(?P<pk>\d+)?/?$', JournalView.as_view(), name='journal'),
 
     #Exam urls
     url(r'^exam/$', exam_list, name='exam'),
